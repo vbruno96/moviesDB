ac14caec69a61807d6112151124bc064917fdebc
Bruno Vinícius <vbruno96@gmail.com>
2023-02-03 15:16:12 -0300

    add types for nativewind and fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,16 @@
 module.exports = {
   content: [
     "./App.{js,jsx,ts,tsx}",
-    "./<custom directory>/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    colors: {
-      'background': "#1E1D20",
-      'primary': "#0C9F7B",
-      'accent': "#662CB2",
+    extend: {
+      colors: {
+        'background': "#1E1D20",
+        'primary': "#0C9F7B",
+        'accent': "#662CB2",
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
