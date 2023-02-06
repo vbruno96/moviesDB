75b9d3c4e70992c185d4f6c50c7c3e141a416876
Bruno Vinícius <vbruno96@gmail.com>
2023-02-05 21:31:56 -0300

    add feature login and configure expo-local-authentication

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,21 +2,40 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import { Login } from "../screens/Login"
 import { Home } from "../screens/Home"
 import { Movie } from "../screens/Movie"
+import { AppContext } from "../AppStore/appContext"
+import { useReducer } from "react"
+import { appReducer } from "../AppStore/appReducer"
+import { initialAppState } from "../AppStore/appState"
 
 export function AppRoutes() {
   const { Navigator, Screen } = createNativeStackNavigator()
+  const [state, dispatch] = useReducer(appReducer, initialAppState)
+
   return (
-    <Navigator
-      screenOptions={{ headerShown: false }}
-    >
-      <Screen 
-        name="home"
-        component={Home}
-      />
-      <Screen 
-        name="movie"
-        component={Movie}
-      />
-    </Navigator>
+    <AppContext.Provider value={{state, dispatch}}>
+      <Navigator
+        screenOptions={{ headerShown: false }}
+      >
+        {
+          state.userIsLogged ? (
+            <>
+              <Screen 
+                name="home"
+                component={Home}
+              />
+              <Screen 
+                name="movie"
+                component={Movie}
+              />
+            </>
+          ) : (
+            <Screen 
+              name="login"
+              component={Login}
+            />
+          )
+        }
+      </Navigator>
+    </AppContext.Provider>
   )
 }
--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,18 +1,73 @@
-import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView } from "react-native"
-import { UserFocus } from "phosphor-react-native"
+import { useEffect, useState } from "react"
+import { View, Text, TextInput, TouchableOpacity, TouchableWithoutFeedback, Keyboard, ScrollView, Alert } from "react-native"
+import * as LocalAuthentication from 'expo-local-authentication'
+import { CircleNotch, Fingerprint, UserFocus } from "phosphor-react-native"
 import Logo from '../assets/logo.svg'
+import { useApp } from "../hooks/useAuth"
+import { fakeAuthUser } from "../lib/fakeApiAuth"
+import { TypeAppActions } from "../AppStore/appActions"
+
+enum BiometricType {
+  FINGERPRINT = 1 as number,
+  FACIAL_RECOGNITION = 2 as number
+}
 
 export function Login() {
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+  const [hasBiometricsHardware, setHasBiometricHardware] = useState(false)
+  const [biometricsType, setBiometricsType] = useState<number[]>()
+  const { dispatch, state } = useApp()
+
+  const btnIsDisabled = user === '' || password === '' ? true : false
+  
+  async function login() {
+    dispatch({type: TypeAppActions.FETCH, payload: {isLoading: true}})
+    await fakeAuthUser(user, password)
+      .then(() => dispatch({type: TypeAppActions.SIGNIN, payload: {userIsLogged: true, isLoading: false}}))
+      .catch(() => Alert.alert('Credênciais inválidas!', 'Usuário ou senha incorreto'))
+  }
+
+  async function checkBiometricsHardware() {
+    await LocalAuthentication.hasHardwareAsync()
+      .then(hasHardware => setHasBiometricHardware(hasHardware))
+    await LocalAuthentication.supportedAuthenticationTypesAsync()
+      .then(types => setBiometricsType(types))
+  }
+
+  async function biometricAuthentication() {
+    await LocalAuthentication.authenticateAsync({
+      promptMessage: "Informe a senha para acessar os filmes",
+      fallbackLabel: "Tente digitar o código"
+    }).then(result => {
+      if (result.success) {
+        dispatch({type: TypeAppActions.SIGNIN, payload: {userIsLogged: true, isLoading: false}})
+      }
+    })
+  }
+
+  useEffect(() => {
+    checkBiometricsHardware()
+  }, [])
+  
   return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
       <ScrollView
         showsVerticalScrollIndicator={false}
         className="bg-background"
       >
-        <View className="flex-1 bg-background justify-center items-center pt-24">
-          <View className="mb-12">
+        <View
+          className="flex-1 bg-background justify-center items-center pt-24"
+        >
+          <View
+            className="mb-12"
+          >
             <Logo />
-            <Text className="text-4xl text-primary">Movies <Text className="text-accent">DB</Text></Text>
+            <Text
+              className="text-4xl text-primary"
+            >
+              Movies <Text className="text-accent">DB</Text>
+            </Text>
           </View>
           
           <View>
@@ -20,29 +75,76 @@ export function Login() {
               className="rounded-full w-80 h-12 mb-9 bg-white/20 bg-opacity-20 text-primary text-center"
               placeholder="Email"
               placeholderTextColor="#0C9F7B"
+              keyboardType="email-address"
+              onChangeText={setUser}
+              value={user}
             />
             <TextInput
               className="rounded-full w-80 h-12 mb-9 bg-white/20 bg-opacity-20 text-primary text-center"
               placeholder="Senha"
               placeholderTextColor="#0C9F7B"
-              disableFullscreenUI={true}
+              keyboardType="visible-password"
+              secureTextEntry
+              onChangeText={setPassword}
+              value={password}
             />
             <TouchableOpacity
               activeOpacity={0.6}
-              className="rounded-full w-80 h-12 mb-12 py-2 bg-primary"
+              className={`rounded-full w-80 h-12 mb-12 py-2 bg-primary ${btnIsDisabled && 'opacity-60'}`}
+              disabled={btnIsDisabled}
+              onPress={() => login()}
             >
               <Text
-              className="text-white text-xl text-center"
+                className="text-white text-xl text-center"
               >
-                Login
+                {
+                  state.isLoading ? (
+                    <CircleNotch
+                      size={20}
+                      color="#FFF"
+                    />
+                  ) : 'Login'
+                }
               </Text>
             </TouchableOpacity>
-            <TouchableOpacity className="items-center">
-            <UserFocus size={40} color="#0C9F7B" weight="regular"/>
-            <Text className="w-[160] text-center text-primary mt-2">
-              Acesse com a indentificação facial
-            </Text>
-            </TouchableOpacity>
+            {
+              hasBiometricsHardware && (
+                <TouchableOpacity 
+                  className="items-center"
+                  onPress={() => biometricAuthentication()}
+                >
+                  {
+                    biometricsType && biometricsType.length > 1 && biometricsType[0] === BiometricType.FINGERPRINT ? (
+                      <>
+                        <Fingerprint
+                          size={40}
+                          color="#0C9F7B"
+                          weight="regular"
+                        />
+                        <Text
+                          className="w-[160] text-center text-primary mt-2"
+                        >
+                          Acesse com a digital
+                        </Text>
+                      </>
+                    ) : (
+                      <>
+                        <UserFocus
+                          size={40}
+                          color="#0C9F7B"
+                          weight="regular"
+                        />
+                        <Text
+                          className="w-[160] text-center text-primary mt-2"
+                        >
+                          Acesse com a indentificação facial
+                        </Text>
+                      </>
+                    )
+                  }
+                </TouchableOpacity>
+              )
+            }
           </View>
         </View>
       </ScrollView>
